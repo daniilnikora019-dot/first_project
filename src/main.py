ba9db073d1992e39f3d8b80c544f7002c236abe0
Daniil Nikora <daniilnikora019@gmail.com>
2026-02-03 21:44:44 +0300

    Создана структура проекта

---
+++ src/main.py
@@ -0,0 +1,18 @@
+# Импорт load_dotenv.
+from dotenv import load_dotenv
+
+# Импорт библиотеки для работы с окружением.
+import os
+
+# Загрузка переменных из .env
+load_dotenv()
+
+# Теперь переменные доступны через os.environ
+author = os.getenv('AUTHOR')
+
+def print_author():
+    # Допишите здесь ваш код
+    print(f'Автор проекта: {author}')
+
+
+print_author()
